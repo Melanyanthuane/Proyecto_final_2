32e9b43fb803c829ea164898dd7076d6fe61d9b1
Melanyanthuane <120582642+Melanyanthuane@users.noreply.github.com>
2022-12-19 22:17:29 -0500

    Add files via upload

---
+++ Proyecto_final2.0/app/pages/8_🗂_Conclusiones.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.write("""###  ¿Se valido o no los resultados? """)
+
+st.write("""Sí se validaron los resultados. """)
+
+st.write("""###  Los resultados validados son: """)
+
+st.write("""###   ¿Es efectivo el método de correlación de Pearson?""")
+
+st.write("""###   Correlación de Pearson y Regresión Lineal, ¿cuál es su relación? """)
+
